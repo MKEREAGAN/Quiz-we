74bebfa379fe812ce3c627423c956f381b55c7b6
MKEREAGAN <monkeelie2014@gmail.com>
2026-07-25 13:51:17 +0000

    Refactor quiz script to include service worker

--- script.js
+++ script.js
@@ -233,5 +233,18 @@ parler.onclick = function () {
 
         speechSynthesis.speak(voix);
     }
-
 };
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then(() => {
+        console.log("Service Worker enregistré");
+      })
+      .catch((err) => {
+        console.log("Erreur :", err);
+      });
+  });
+}
